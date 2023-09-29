461142d848bcbff716f48aaffddadf45a8e4545f
Adrián <58742147+dip000@users.noreply.github.com>
2023-09-29 17:55:59 -0600

    works boringly well

--- WorldPainter/image_processing.gd
+++ WorldPainter/image_processing.gd
@@ -5,6 +5,7 @@ var material:ShaderMaterial = get_active_material(0)
 var base_texture:ImageTexture
 #var base_image:Image = preload("res://Images/base_texture.png").get_image()
 var splash_end_image:Image = preload("res://Images/splash_end.png").get_image()
+var splash_end_mask_image:Image = preload("res://Images/splash_end_mask.png").get_image()
 
 const SIZE:Vector2 = Vector2(512, 512)
 const HALF_SIZE:Vector2 = Vector2(256, 256)
@@ -21,7 +22,7 @@ func splash_at(splash_position:Vector2, slplash_scale:float):
 	material.set_shader_parameter("scale", slplash_scale)
 	
 	# Start and wait for shader animation to finish
-	var animation:MethodTweener = create_tween().tween_method(_set_reveal_factor, 0.0, 1.0, 2.0).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	var animation:MethodTweener = create_tween().tween_method(_set_reveal_factor, 0.0, 0.92, 2.0).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 	await animation.finished
 	
 	# Update baked texture and reset shader
@@ -39,8 +40,13 @@ func _bake_splash_into_base_texture(p:Vector2, s:float):
 	p *= SIZE #move relative to pixel size
 	p += HALF_SIZE * (1 - s) #move relative to center
 	
+	# Using the source images will lower their resolution
+	var splash:Image = splash_end_image.duplicate()
+	var mask:Image = splash_end_mask_image.duplicate()
+	splash.resize( size.x, size.y )
+	mask.resize( size.x, size.y )
+	
 	# Draws the last frame of the splash animation on top of 'base_texture'
-	splash_end_image.resize( size.x, size.y )
-	base_image.blit_rect( splash_end_image, FULL_RECT, p )
+	base_image.blit_rect_mask( splash, mask, FULL_RECT, p )
 	base_texture.update( base_image )
 	print("FINISHED!")
