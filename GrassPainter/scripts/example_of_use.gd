a40c223d9ec65dc080e3d295947232005b05180a
Adrián <58742147+dip000@users.noreply.github.com>
2023-10-12 22:04:27 -0600

    Pwetty gwass <3

--- GrassPainter/scripts/example_of_use.gd
+++ GrassPainter/scripts/example_of_use.gd
@@ -1,7 +1,11 @@
+# EXAMPLE OF USE
+# This script will only find the mouse position in PaintSurface and call 'splash_at()'
+# Refer to PaintSurface for shader logic
 extends Node3D
 
+@export var splash_color:Color ## Use alpha to blend with paint surface 
 @onready var cam:Camera3D = $Camera3D
-@onready var shader_helper:ShaderHelper = $Ground
+@onready var paint_surface:PaintSurface = $Ground
 @onready var space_state:PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
 
 const INTERACT_RADIUS:int = 15
@@ -10,13 +14,13 @@ var query := PhysicsRayQueryParameters3D.new()
 
 func _ready():
 	query.set_collide_with_areas(true)
-	
+
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		var result:Dictionary = _detect_from_cam_to_mouse()
 		if result:
-			var pos:Vector2 = Vector2(result.position.x, result.position.z) / shader_helper.mesh.size
-			shader_helper.splash_at(pos, 0.3)
+			var pos:Vector2 = Vector2(result.position.x, result.position.z) / paint_surface.mesh.size
+			paint_surface.splash_at( pos, 0.3, splash_color )
 
 func _detect_from_cam_to_mouse() -> Dictionary:
 	query.from = cam.global_position
